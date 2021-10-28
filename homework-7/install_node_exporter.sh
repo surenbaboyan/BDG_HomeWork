9a9b28f9c639f6e79f3f2a7ae56a553d075428ae
Suren Baboyan <39882067+surenbaboyan@users.noreply.github.com>
2021-10-28 17:29:16 +0400

    Delete install_node_exporter.sh

--- homework-7/install_node_exporter.sh
+++
@@ -1,8 +0,0 @@
-mkdir /home/$USER/temp
-tar xzf node_exporter-1.2.2.linux-amd64.tar.gz -C /home/$USER/temp
-sudo mv -v /home/$USER/temp/node_exporter-1.2.2.linux-amd64/node_exporter /usr/local/bin/
-sudo chown root:root /usr/local/bin/node_exporter
-sudo cp node-exporter.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start node-exporter.service
-sudo systemctl enable node-exporter.service
