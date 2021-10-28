d9a9b0e6fd7cad9e325513c356c204ed9ef2dc50
Suren Baboyan <suren.baboyan@gmail.com>
2021-10-28 14:28:48 +0400

    Install Prometheus Scrypt

--- homework-7/install_prometheus_scrypt.sh
+++ homework-7/install_prometheus_scrypt.sh
@@ -1,13 +1,13 @@
 sudo apt update
 mkdir /home/$USER/temp
-cp prometheus-2.30.3.linux-amd64.tar.gz /home/$USER/tmp
+cp prometheus-2.30.3.linux-amd64.tar.gz /home/$USER/temp
 sudo cp prometheus.service /etc/systemd/system/
-tar /home/$USER/tmp xvf prometheus-2.30.3.linux-amd64.tar.gz
+tar xvf /home/$USER/temp/prometheus-2.30.3.linux-amd64.tar.gz
 mkdir /etc/prometheus
 mkdir /var/lib/prometheus
-mv /home/$USER/tmp/prometheus-2.30.3.linux-amd64/prometheus /home/$USER/tmp/prometheus-2.30.3.linux-amd64/promtool /usr/local/bin/
-mv /home/$USER/tmp/prometheus-2.30.3.linux-amd64/consoles/ /home/$USER/tmp/prometheus-2.30.3.linux-amd64/console_libraries/ /etc/prometheus/
-mv /home/$USER/tmp/prometheus-2.30.3.linux-amd64/prometheus.yml /etc/prometheus/prometheus.yml
+mv /home/$USER/temp/prometheus-2.30.3.linux-amd64/prometheus /home/$USER/temp/prometheus-2.30.3.linux-amd64/promtool /usr/local/bin/
+mv /home/$USER/temp/prometheus-2.30.3.linux-amd64/consoles/ /home/$USER/temp/prometheus-2.30.3.linux-amd64/console_libraries/ /etc/prometheus/
+mv /home/$USER/temp/prometheus-2.30.3.linux-amd64/prometheus.yml /etc/prometheus/prometheus.yml
 sudo groupadd --system prometheus
 sudo useradd -s /sbin/nologin --system -g prometheus prometheus
 sudo chown -R prometheus:prometheus /etc/prometheus/  /var/lib/prometheus/
